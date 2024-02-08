8b6a0c7342bbc29deb68440aaebef00cc8a3cbf3
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2024-02-08 13:41:36 +0530

    add weatherly project

---
+++ components/CalloutCard.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import {
+  CheckCircleIcon,
+  ExclamationTriangleIcon,
+} from "@heroicons/react/24/solid";
+import { Callout } from "@tremor/react";
+
+type Props = {
+  message: string;
+  warning?: boolean;
+};
+
+function CalloutCard({ message, warning }: Props) {
+  return (
+    <Callout
+      className="mt-4"
+      title={message}
+      icon={warning ? ExclamationTriangleIcon : CheckCircleIcon}
+      color={warning ? "rose" : "teal"}
+    />
+  );
+}
+
+export default CalloutCard;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.weatherbit.io",
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@tremor/react"],
+  },
+};
 
 export default nextConfig;
